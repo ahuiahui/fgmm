489748b4a17941b4e12d0a78a5a7e29d4329aa53
fdhalluin <fdhalluin@lasapc26>
2010-04-23 18:44:52 +0200

    now auto parse the input test file for right dimension, and num of states

--- tests/test_em.c
+++ tests/test_em.c
@@ -2,45 +2,89 @@
 #include <stdio.h>
 #include <sys/time.h> // gettimeofday .. 
 #include <time.h>
+#include <string.h>
 
 #include "em.h"
-//#include "gmm.h"
 
-#define N_STATES 6
-#define DIM 3
-#define N_DATA 10000
+static char usage[] = "test_em infile #states \n infile text with space separated floats \n # state number of gaussians .. ";
 
 int main(int argc,char ** argv)
 {
-  srand(time(NULL));
-  float *  data;
-  data = (float *) malloc(DIM*N_DATA*sizeof(float));
-  
-  struct gmm GMM;
-  gmm_alloc(&GMM,N_STATES,DIM);
-
-  int state_i=0;
-  int i=0,j=0;
-  /* random initialization */ 
-  for(state_i=0;state_i<N_STATES;state_i++)
+  if(argc < 3)
     {
-      GMM.gauss[state_i].prior = 1./N_STATES;
-      for(j=0;j<DIM;j++)
-	GMM.gauss[state_i].mean[j] = ((float)rand()/RAND_MAX)*2. - 1.;
-      //dump(&GMM[state_i]);
+      printf("%s",usage);
+      exit(0);
     }
+
+  char * filename = argv[1];
+  int n_states = atoi(argv[2]);
+
+  srand(time(NULL));
+
+  //  data = (float *) malloc(DIM*N_DATA*sizeof(float));
+
   FILE * infile;
-  infile=fopen("test.txt","r");
+
+  infile=fopen(filename,"r");
   if(infile == NULL)
-    printf("can't open file -- will gently segfault later :)) \n");
-  float * pdata = data;
-  for(i=0;i<N_DATA;i++)
     {
-      for(j=0;j<DIM;j++)
+      printf("can't open file\n");
+      exit(0);
+    }
+
+
+  char line[1024];
+  char *pline;
+
+  int n_data=0;
+  int dim=0;
+
+  int i=0;
+  while(!feof(infile))
+    {
+      fgets(line,1024,infile);
+      if(dim==0)
+	{
+	  pline = strtok(line," ");
+	  while(pline != NULL)
+	    {
+	      dim++;
+	      pline=strtok(NULL," ");
+	    }
+	}
+      n_data++;
+    }
+  n_data--;
+  dim--;
+  rewind(infile);
+  printf("%d , %d\n",n_data,dim);
+  float *  data;
+  data = (float *) malloc(sizeof(float) * dim * n_data);
+  float * pdata = data;
+  
+  int j;
+  for(i=0;i<n_data;i++)
+    {
+      for(j=0;j<dim;j++)
 	{
 	  fscanf(infile,"%f",pdata);
 	  pdata++;
 	}
+    }
+
+  
+  struct gmm GMM;
+  gmm_alloc(&GMM,n_states,dim);
+
+  int state_i=0;
+  //int i=0,j=0;
+  /* random initialization */ 
+  for(state_i=0;state_i<n_states;state_i++)
+    {
+      GMM.gauss[state_i].prior = 1./n_states;
+      for(j=0;j<dim;j++)
+	GMM.gauss[state_i].mean[j] = ((float)rand()/RAND_MAX)*2. - 1.;
+      //dump(&GMM[state_i]);
     }
   
   printf("end loading file\n");
