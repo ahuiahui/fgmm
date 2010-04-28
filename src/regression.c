2130ddbe2f72bc21afa6c485698ef4a49ea099ec
fdhalluin <fdhalluin@lasapc26>
2010-04-28 18:15:24 +0200

    adding regression structures and alloc function

--- src/regression.c
+++ src/regression.c
@@ -1,3 +1,16 @@
+#include "gaussian.h"
+#include "gmm.h"
+
+struct gaussian_reg {
+  struct gaussian * gauss;
+  struct gaussian * subgauss; // input subgaussian Used to compute the weight of this
+  int * input_dim;
+  int * output_dim;
+  int input_len;
+  int output_len;
+  float * reg_matrix; // store in->out A matrix 
+};
+
 
 struct gmm_reg {
   struct gmm * model;
@@ -5,13 +18,51 @@ struct gmm_reg {
   int * output_dim;
   int input_len;
   int output_len;
+  struct gaussian_reg * subgauss;
 };
 
-void gmm_init_reg(struct gmm_reg * reg,
-		  struct gmm * gmm,
-		  int input_len, int * input_dim,
-		  int output_len, int * output_dim)
+
+/*
+void gmm_regression_init(struct gmm_reg * reg)
 {
+}
+
+void gmm_regression_init_g(struct gaussian_reg * gr)
+{
+  gaussian_get_subgauss(gr->gauss,&(gr->subgauss),
+			input_dim,input_len);
+  // reg_matrix = (Sigma^00)-1 * (Sigma^0i)
+  if(reg_matrix != NULL)
+    free(reg_matrix);
+  reg_matrix =(float*)  malloc(sizeof(float) * input_len * output_len);
+  struct smat * fcov = gr->gauss->covar;
+  for(j=0;j<output_len;j++)
+    {
+      for(i=0;i<input_len;i++)
+	{
+	  reg_matrix[j * input_len + i] = smat_get_value(fcov,output_dim[j],input_dim[i]);
+	}
+    }
+}
+*/
+
+
+/**
+ * should be like gmm_regression_alloc() 
+ *                gmm_regression_alloc_simple()
+ *
+ * then gmm_regression_init() 
+ *                         _g() for single gaussian 
+ */
+void gmm_regression_alloc(struct gmm_reg ** regression,
+			  struct gmm * gmm,
+			  int input_len, int * input_dim,
+			  int output_len, int * output_dim)
+{
+
+  struct gmm_reg * reg = *regression;
+  reg = (struct gmm_reg*) malloc(sizeof(struct gmm_reg));
+
   int i = 0;
   reg->model = gmm;
   reg->input_len = input_len;
@@ -28,9 +79,9 @@ void gmm_init_reg(struct gmm_reg * reg,
  * initialise a regression structure , considering that 
  * first input_len dimensions are input and the rest the output
  */
-void gmm_init_reg_simple(struct gmm_reg * reg,
-			 struct gmm * gmm,
-			 int input_len)
+void gmm_regression_alloc_simple(struct gmm_reg ** regression,
+				 struct gmm * gmm,
+				 int input_len)
 {
   int output_len = gmm->dim - input_len;
   int inputs[input_len];
@@ -44,9 +95,11 @@ void gmm_init_reg_simple(struct gmm_reg * reg,
     {
       outs[i] = input_len + i;
     }
-  gmm_init_reg(reg,gmm,input_len,inputs,output_len,outs);
+  gmm_regression_alloc(regression,gmm,input_len,inputs,output_len,outs);
 }
 
+/*
 void gmm_regression(struct gmm * gmm, 
 		    float * input,
 		    float * output)
+*/
