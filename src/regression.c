1b5a48603ba8cde194d7423d704efd4cff454470
fdhalluin <fdhalluin@lasapc26>
2010-04-30 13:22:44 +0200

    regression code is complete, compiles, and not tested at all .. needs : proper header, test

--- src/regression.c
+++ src/regression.c
@@ -1,5 +1,8 @@
 #include "gaussian.h"
 #include "gmm.h"
+#include <stdlib.h>
+#include <float.h>
+#include <assert.h>
 
 struct gaussian_reg {
   struct gaussian * gauss;
@@ -22,29 +25,86 @@ struct gmm_reg {
 };
 
 
-/*
-void gmm_regression_init(struct gmm_reg * reg)
-{
-}
 
 void gmm_regression_init_g(struct gaussian_reg * gr)
 {
-  gaussian_get_subgauss(gr->gauss,&(gr->subgauss),
-			input_dim,input_len);
+  int i,j;
+  gr->subgauss = (struct gaussian *) malloc(sizeof(struct gaussian));
+  gaussian_init(gr->subgauss,gr->input_len);
+  gaussian_get_subgauss(gr->gauss,gr->subgauss,
+			gr->input_len,gr->input_dim);
   // reg_matrix = (Sigma^00)-1 * (Sigma^0i)
-  if(reg_matrix != NULL)
-    free(reg_matrix);
-  reg_matrix =(float*)  malloc(sizeof(float) * input_len * output_len);
+  if(gr->reg_matrix != NULL)
+    free(gr->reg_matrix);
+  gr->reg_matrix =(float*)  malloc(sizeof(float) * gr->input_len * gr->output_len);
   struct smat * fcov = gr->gauss->covar;
-  for(j=0;j<output_len;j++)
+  for(j=0;j<gr->output_len;j++)
     {
-      for(i=0;i<input_len;i++)
+      for(i=0;i<gr->input_len;i++)
 	{
-	  reg_matrix[j * input_len + i] = smat_get_value(fcov,output_dim[j],input_dim[i]);
+	  gr->reg_matrix[j * gr->input_len + i] = smat_get_value(fcov,
+							 gr->output_dim[j],
+							 gr->input_dim[i]);
 	}
     }
 }
-*/
+
+
+void gmm_regression_init(struct gmm_reg * reg)
+{
+  int state=0;
+  for(;state < reg->model->nstates ; state++)
+    {
+      gmm_regression_init_g(&reg->subgauss[state]);
+    }
+}
+
+
+
+void gmm_regression_gaussian(struct gaussian_reg* gr, float * inputs,float * result)
+{
+  /*float result[gr->output_len];*/
+  float tmp[gr->input_len]; 
+  float tmp2[gr->input_len]; 
+  /* OPT : this computation is also done for the 
+     subgauss pdf (ie weight of the gaussian in the regression .. */
+  smat_tforward(gr->subgauss->covar_cholesky,inputs,tmp2);
+  smat_tbackward(gr->subgauss->covar_cholesky,tmp2,tmp);
+  int j=0,i=0;
+  for(;i<gr->output_len;i++)
+    {
+      result[i] = gr->subgauss->mean[i];
+      for(;j<gr->input_len;j++)
+	{
+	  result[i] += gr->reg_matrix[i * gr->input_len + j]*tmp[j];
+	}
+    }
+}
+
+
+void gmm_regression(struct gmm_reg * reg, float * inputs)
+{
+  float weight = 0;
+  float result[reg->output_len];
+  float tmp[reg->output_len];
+  float likelihood = 0;
+  int state = 0;
+  int i=0;
+  for(i=0;i<reg->output_len;i++)
+    result[i] = 0;
+     
+  for(;state<reg->model->nstates;state++)
+    {
+      weight = gaussian_pdf(reg->subgauss[state].subgauss,inputs);
+      gmm_regression_gaussian(&reg->subgauss[state],inputs,tmp);
+      for(i=0;i<reg->output_len;i++)
+	result[i] += weight*tmp[i];
+      likelihood += weight;
+    }
+  assert(likelihood > FLT_MIN);
+  for(i=0;i<reg->output_len;i++)
+    result[i] /= likelihood;
+}
 
 
 /**
@@ -73,6 +133,17 @@ void gmm_regression_alloc(struct gmm_reg ** regression,
   reg->output_dim = (int*) malloc(sizeof(int)*output_len);
   for(i=0;i<output_len;i++)
     reg->output_dim[i] = output_dim[i]; 
+
+  int state=0;
+  reg->subgauss = (struct gaussian_reg*) malloc(sizeof(struct gaussian_reg) * reg->model->nstates);
+  for(;state < reg->model->nstates ; state++)
+    {
+      reg->subgauss[state].gauss = &gmm->gauss[state];
+      reg->subgauss[state].input_len = input_len;
+      reg->subgauss[state].output_len = output_len;
+      reg->subgauss[state].output_dim = reg->output_dim;
+      reg->subgauss[state].input_dim = reg->input_dim;
+    }
 }
 
 /**
