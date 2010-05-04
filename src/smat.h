ddc402c3ead8d0b4740fd74cf5e4e61c329feac0
fdhalluin <fdhalluin@lasapc26>
2010-05-04 16:01:41 +0200

    adding docs to smat headers

--- src/smat.h
+++ src/smat.h
@@ -3,27 +3,62 @@
     
     florent.dhalluin@epfl.ch */
     
+/**
+ * stores Symetric matrices in a efficient way : 
+ *   data is stored row first order such as for a 3x3 symetrical matrix 
+ *     1 2 3
+ * m = 2 4 5    ->  m->_ = [ 1 2 3 4 5 6] 
+ *     3 5 6
+ */
 
 struct smat {
-  float * _;
-  int dim;
-  int _size;
+  float * _; /* data is actually stored here */
+  int dim;   /* dimensionnality of the matrix */
+  int _size; /* actual size of the data pointer dim * (dim+1) /2 */
 };
 
-inline void smat_multv(const struct smat* m, const float * v,float * out);
-inline void smat_multf(struct smat* m,const float *f);
 
-/* allocate memory for smat if mat == NULL 
-   and zero the matrix in all cases */
+
+/**
+ * allocate memory for smat if *mat == NULL 
+ * and zero the matrix in all cases 
+ *
+ * Use this function to allocate memory for smat structures
+ */
 void smat_zero(struct smat ** mat,int dim);
 
+
+/**
+ *  free the memory used by the matrix 
+ *  (allocated by smat_zero) 
+ *  *mat is set to NULL after that
+ */
+void smat_free(struct smat ** mat);
+
+/**
+ *  Matrix x Vector multiplication 
+ *
+ *  out = m * v 
+ */
+inline void smat_multv(const struct smat* m, const float * v,float * out);
+
+/**
+ *  _in place _ Matrix x float  multiplication 
+ */
+inline void smat_multf(struct smat* m,const float *f);
+
+/**
+ * get the value at row col if the matrix were stored as 
+ * square matrix 
+ */
 float smat_get_value(struct smat * mat,int row,int col);
+
+/**
+ * return the symetrical matrix considering only 
+ * a given subset of dimensions 
+ */
 void smat_get_submatrix(struct smat * mat , struct smat * res,
 			int n_dims, int * dims);
-
-/* free the memory used by the matrix 
-   (allocated by smat_zero) */
-void smat_free(struct smat ** mat);
 
 /* fill the mat with identity  /!\ you must first 
  allocate memory (wigh smat_zero for ex .. ) */
@@ -68,9 +103,19 @@ inline void smat_tbackward(const struct smat * upper, float * b, float * y);
  * computes sesquilinear form :
  *   (x - bias)^T Sigma^-1 (x-bias) 
  * given two vectors x and bias and the cholesky decomposition of Sigma 
- * ichol 
+ * ichol is actualy the cholesky decomposition of Sigma, where its value 
+ * on the diagonal are inverted ... 
  */
 inline float smat_sesq(struct smat * ichol,const float * bias,const float * x);
+
+/**
+ * compute the weighted covariance matrix of a given dataset
+ * 
+ * mainly used in the maximisation step of EM 
+ * weight are the weights of each element of data
+ * ndata : number of elements in data (data's size is ndata*cov->dim ) 
+ * mean : the computed weighted mean (must be alloc'd before , its size is cov->dim ) 
+ */
 
 float smat_covariance(struct smat * cov, 
 		      int ndata, 
