6b6d233ccb4168d2e78ecf04fc4aa5ce2314b813
Florent D'halluin <florent.dhalluin@epfl.ch>
2010-09-22 17:58:30 +0200

    update python test script to have nice plots ..

--- python/test.py
+++ python/test.py
@@ -1,10 +1,19 @@
-
+# 
+# test script for python interface to fgmm 
+#
+# should plot noisy sine data (as black dots) 
+# and try sampling regression (red line ) 
+# and sampling from the sine data ( blue dots ) 
+# 
 import numpy as np
 import fgmm
 from matplotlib import pyplot
 
+nstates = 4
+
 t = np.linspace(0,6,1000)
-#t = np.arange(2000)
+#t = np.hstack([t[:300], t[600:] ])
+#print t.shape
 y = np.sin(t) + np.random.random(1000)*.5
 
 pyplot.plot(t,y,'k.')
@@ -13,11 +22,11 @@ dat = np.vstack([t,y]).T
 
 dat = dat.astype(np.dtype('f4')) # this is the messy stuff .. 
 
-g = fgmm.GMM(20,2)
+g = fgmm.GMM(nstates,2)
 
-g.init(dat)
+g.kmeans(dat)
 
-g.Em(dat)
+g.Em(dat,1e-3,fgmm.COVARIANCE_SPHERE)
 
 g.Dump()
 
@@ -36,8 +45,17 @@ s=np.vstack([g.Draw() for _ in range(1000)])
 pyplot.plot(inp,r,'r-',linewidth=3)
 pyplot.plot(s[:,0],s[:,1],'b.')
 
-print g.GetMean(1)
-print g.GetCovariance(1)
+_t = np.linspace(0,2*np.pi,50)
+circle = np.vstack( [np.cos(_t), np.sin(_t)])
+for i in range(nstates ) :
+    sig = g.GetCovariance(i)
+    print sig
+    el = np.dot(np.linalg.cholesky(sig),circle).T
+    print el.shape
+    print g.GetMean(i)
+    el += g.GetMean(i)
+    pyplot.plot(el[:,0],el[:,1],'k-',linewidth=3)
+
 pyplot.show()
 
 
